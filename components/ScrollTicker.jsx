7b23ab474842faf8c144b4bab449412cae4e5dc7
yube01 <114811769+yube01@users.noreply.github.com>
2023-02-28 19:43:28 +0545

    scrollticker demo added

---
+++ components/ScrollTicker.jsx
@@ -0,0 +1,17 @@
+import React from 'react'
+
+const ScrollTicker = () => {
+  return (
+    <div className="scroll">
+        <div className="container">
+            <marquee behavior="" direction="">
+                <span>
+                For any issues, please fill the form by clicking on the Support button on our App or website. We will get back to you shortly. For Customer Service, Call us at: 01-5522566
+                </span>
+            </marquee>
+        </div>
+    </div>
+  )
+}
+
+export default ScrollTicker
