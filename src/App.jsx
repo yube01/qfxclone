804b74971933061889c4e3cfe3280145a50941ac
yube01 <114811769+yube01@users.noreply.github.com>
2023-03-01 18:57:36 +0545

    font added

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,7 @@
 import { useState } from 'react'
-import reactLogo from './assets/react.svg'
+
 import './App.css'
-import ScrollTicker from '../components/ScrollTicker'
+import ScrollTicker from '../components/scrollticker/ScrollTicker'
 
 function App() {
   const [count, setCount] = useState(0)
