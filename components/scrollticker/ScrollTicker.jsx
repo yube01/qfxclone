70865f0d6c2ac0e36f8b73ea21aca4b548bf876c
yube01 <114811769+yube01@users.noreply.github.com>
2023-03-01 19:48:41 +0545

    pop up design complete

--- components/scrollticker/ScrollTicker.jsx
+++ components/scrollticker/ScrollTicker.jsx
@@ -1,35 +1,34 @@
-import React, { useState } from 'react'
-import "./scrolltiker.css"
-import cross from "../../img/cross.png"
+import React, { useState } from "react";
+import "./scrolltiker.css";
+import cross from "../../img/cross.png";
 
 const ScrollTicker = () => {
+  const [closeTicker, setCloseTicker] = useState(false);
 
-  const [closeTicker , setCloseTicker] = useState(false)
-
-
-  const handleClick = ()=>{
-    setCloseTicker(true)
-  }
+  const handleClick = () => {
+    setCloseTicker(!closeTicker);
+  };
 
   return (
-    <div className="scroll">
-        <div className="container">
+    <>
+      { !closeTicker?
+        <div className="scroll">
+          <div className="container">
             <marquee behavior="" direction="">
-                <span className='text'>
-                For any issues, please fill the form by clicking on the Support button on our App or website. We will get back to you shortly. For Customer Service, Call us at: 01-5522566
-                </span>
-                  
+              <span className="text">
+                For any issues, please fill the form by clicking on the Support
+                button on our App or website. We will get back to you shortly.
+                For Customer Service, Call us at: 01-5522566
+              </span>
             </marquee>
-          
-           
-        </div>
-        <div className="img">
-        <img src={cross} alt="" onClick={handleClick}/>
-        </div>
-      
-     
-    </div>
-  )
-}
+          </div>
+          <div className="img">
+            <img src={cross} alt="" onClick={handleClick} />
+          </div>
+        </div>:""
+      }
+    </>
+  );
+};
 
-export default ScrollTicker
+export default ScrollTicker;
